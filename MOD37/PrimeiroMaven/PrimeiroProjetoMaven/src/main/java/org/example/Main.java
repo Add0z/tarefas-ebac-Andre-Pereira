6e89cbf17f3e2023efd294444aa5dbf1ed7f0530
Add0z <andre_ps@hotmail.com>
2023-06-09 15:26:02 -0300

    mod 37

---
+++ MOD37/PrimeiroMaven/PrimeiroProjetoMaven/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // Press Opt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome!");
+
+        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
+
+    }
+}
